a714ba05742f664be5e610bc9c3e3b164cfac7d5
cabre94 <cabre94@hotmail.com>
2021-03-21 12:45:18 -0300

    Agregue simulacion y corregi error

--- Practica_2/ej_01.py
+++ Practica_2/ej_01.py
@@ -10,12 +10,18 @@ GitLab: https://gitlab.com/cabre94
 Description: 
 """
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from scipy.signal import find_peaks
 import seaborn as sns
 sns.set()
+
+SAVE_PATH = os.path.join("Figuras", "ej_01")
+# SAVE_PATH = os.path.join("Figuras", "Ej_06")
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
 
 # Constantes
 V_Na = 50       # mV
@@ -56,11 +62,11 @@ def s_inf(V):
     return 0.5 * (1 + np.tanh(0.2*V))
 
 
-def Hogdkin_Huxley(z, t, g_syn, V_pre, I=0, V_sync=0, tau=3, m_inf=False, hn_cte=False):
+def Hogdkin_Huxley(z, t, g_syn, V_pre, I_ext=0, V_syn=0, tauS=3, m_inf=False, hn_cte=False):
     V, m, h, n, s = z
 
     # dsdt
-    dsdt = (s_inf(V_pre) -s) * tau
+    dsdt = (s_inf(V_pre) - s) / tauS
 
     # dmdt
     a, b = a_m(V), b_m(V)
@@ -86,8 +92,82 @@ def Hogdkin_Huxley(z, t, g_syn, V_pre, I=0, V_sync=0, tau=3, m_inf=False, hn_cte
     I_Na = g_Na * np.power(m,3) * h * (V - V_Na)
     I_K = g_K * np.power(n,4) * (V - V_K)
     I_L = g_l * (V - V_l)
-    I_syn = -g_syn * s * (V - V_sync)
-    dVdt = I + I_syn - I_Na - I_K - I_L
+    I_syn = -g_syn * s * (V - V_syn)
+    # print(I_syn)
+
+    dVdt = I_ext + I_syn - I_Na - I_K - I_L
     dVdt /= C
 
-    return [dVdt, dmdt, dhdt, dndt]
+    return [dVdt, dmdt, dhdt, dndt, dsdt]
+
+def simulacion(n_iter=2000, V_syn=0, I_ext=15e-3, tauS=3):
+
+    g_syn_list = np.linspace(0, 2, 51)
+    # g_syn_list = np.linspace(0, 2, 101)
+
+    for g_syn in g_syn_list:
+
+        # z_0_1 = [-20, 0.1, 0.5, 0.3, 0.1]
+        # z_0_2 = [-30, 0.1, 0.5, 0.3, 0.1]
+
+        # z_0_1 = [-20, 0.1, 0.5, 0.3, 0.1] 50 cprriente
+        # z_0_2 = [-30, 0.1, 0.5, 0.3, 0.1]
+
+        z_0_1 = [-20,  0.1, 0.7, 0.1, 1]        # Esto con 10 da lindo, hay que ver con V!=0
+        z_0_2 = [-30, 0.1, 0.3, 0.3, 1]
+        
+        # z_0_1 = [-30,  0.1, 0.4, 0.3, 0.1]
+        # z_0_2 = [-80, 0.1, 0.4, 0.1, 0.1]
+
+        print("g_syn: {}".format(g_syn))
+
+        n_iter = 10000
+        t = np.linspace(0, 1000, n_iter)
+        # t = np.linspace(0, 200, n_iter)
+        
+        V_1, V_2 = np.zeros_like(t), np.zeros_like(t)
+        m_1, m_2 = np.zeros_like(t), np.zeros_like(t)
+        h_1, h_2 = np.zeros_like(t), np.zeros_like(t)
+        n_1, n_2 = np.zeros_like(t), np.zeros_like(t)
+        s_1, s_2 = np.zeros_like(t), np.zeros_like(t)
+        
+        V_1[0], m_1[0], h_1[0], n_1[0], s_1[0] = z_0_1
+        V_2[0], m_2[0], h_2[0], n_2[0], s_2[0] = z_0_2
+
+        # np.full(n_iter, I_ext)
+
+        for i in range(1,n_iter):
+            t_span = [t[i - 1], t[i]]
+
+            # z_1 = odeint(Hogdkin_Huxley, z_0_1, t_span, args=(g_syn, V_2[-1], I_ext, V_syn, tauS,))
+            # z_2 = odeint(Hogdkin_Huxley, z_0_2, t_span, args=(g_syn, V_1[-1], I_ext, V_syn, tauS,))
+            z_1 = odeint(Hogdkin_Huxley, z_0_1, t_span, args=(g_syn, z_0_2[0], I_ext, V_syn, tauS,))
+            z_2 = odeint(Hogdkin_Huxley, z_0_2, t_span, args=(g_syn, z_0_1[0], I_ext, V_syn, tauS,))
+            
+            z_0_1 = z_1[1]
+            z_0_2 = z_2[1]
+
+            V_1[i], m_1[i], h_1[i], n_1[i], s_1[i] = z_0_1
+            V_2[i], m_2[i], h_2[i], n_2[i], s_2[i] = z_0_2
+        
+        plotear(t, V_1, V_2, g_syn)
+
+def plotear(t, V_1, V_2, g_syn):
+    plt.plot(t, V_1, label="Neurona 1")
+    plt.plot(t, V_2, label="Neurona 2")
+    plt.ylabel("Voltaje (mV)")
+    plt.xlabel("Tiempo (ms)")
+    # sns.despine(trim=True)
+    plt.legend(loc='best')
+    file_name = os.path.join(SAVE_PATH, "{:.2f}.pdf".format(g_syn))
+    plt.savefig(file_name, format='pdf')
+    plt.tight_layout()
+    # plt.show()
+    plt.close()
+
+if __name__ == "__main__":
+    # simulacion()
+    # simulacion(I_ext=15e-2)
+    # simulacion(I_ext=10)
+    simulacion(I_ext=10, V_syn=-80)
+    # simulacion(V_syn=-80)
