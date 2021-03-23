305a6653c86906b4988af01e6c4f1bfed3d035ab
cabre94 <cabre94@hotmail.com>
2021-03-23 15:35:04 -0300

    Primer commit

---
+++ Practica_3/ej.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 23-03-21
+File: ej.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
