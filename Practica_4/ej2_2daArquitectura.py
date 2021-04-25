af2714d4409929a31d5f99e1543a26513b1587ca
cabre94 <cabre94@hotmail.com>
2021-04-25 13:05:43 -0300

    Agrego ejercicio 1 con la 2da arquitectura

---
+++ Practica_4/ej2_2daArquitectura.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+date: 22-04-21
+File: ej2_2daArquitectura.py
+Author : Facundo Martin Cabrera
+Email: cabre94@hotmail.com facundo.cabrera@ib.edu.ar
+GitHub: https://github.com/cabre94
+GitLab: https://gitlab.com/cabre94
+Description: 
+"""
+
+
